935588ea94d237247ef93074db64217b06d39d29
arifwp <arifwahyu696@gmail.com>
2024-03-24 21:20:35 +0700

    bug fix

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function NewTask({ onAdd }) {
 
-    const [enteredTask, setEnteredTask] = useState();
+    const [enteredTask, setEnteredTask] = useState('');
 
     function handleChange(event) {
         setEnteredTask(event.target.value);
@@ -18,7 +18,7 @@ export default function NewTask({ onAdd }) {
             <input
                 type="text"
                 className="w-64 px-2 py-1 rounded-sm bg-stone-200"
-                onClick={handleChange}
+                onChange={handleChange}
                 value={enteredTask}
             />
             <button
